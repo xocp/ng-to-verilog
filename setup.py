1c77cacde7abd87cfd59e99b189eef4d3f6257bf
Patrick Cox <patrick.cox@gmail.com>
2024-08-05 21:30:37 -0700

    Build settings

--- setup.py
+++ setup.py
@@ -1,11 +1,5 @@
 from setuptools import setup
 
 setup(
-   name='ng_to_verilog',
-   version='1.0',
-   description='Convert Nandgame json exports into Verilog',
-   author='Patrick Cox',
-   author_email='patrick.cox@gmail.com',
-   packages=['ng_to_verilog'],
-   install_requires=['Jinja2', 'MarkupSafe', 'setuptools', 'wheel']
+   packages=['ng_to_verilog']
 )
