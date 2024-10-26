c4b15acedd7de93f1aba869a45a9539eb659e654
Patrick Cox <patrick.cox@gmail.com>
2024-10-26 16:57:42 -0700

    unit test updates

--- tests/test_load.py
+++ tests/test_load.py
@@ -1,10 +1,5 @@
-import unittest
-from .context import ng_to_verilog as ngv
+import ng_to_verilog as ngv
 
-class TestLoad(unittest.TestCase):
-    def test_load_ng_data(self):
-        ngData = ngv.load_ng_data("samples/resources/nandgame_export.json")
-        self.assertIsNotNone(ngData)
-
-if __name__ == '__main__':
-    unittest.main()
+def test_load_ng_data():
+	ngData = ngv.load_ng_data("samples/resources/nandgame_export.json")
+	assert ngData is not None
